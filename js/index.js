aabd3c75b052433e362ea0ee8da39829b27222d7
Thiago Silva <uzmaki_thiago@outlook.com>
2022-05-17 15:42:58 -0300

    funcoes js para fazer consultas no banco de dados

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Grafico pizza
 let dvChartDunut = document.querySelector("#chart-dunut")
 let optionsDunut = {
   chart: {
@@ -14,6 +15,7 @@ let optionsDunut = {
 let chartDunit = new ApexCharts(dvChartDunut, optionsDunut);
 chartDunit.render();
 
+//grafico barras
 let dvChartBar = document.querySelector("#chart-bar")
 let optionsBar = {
   chart: {
@@ -30,6 +32,7 @@ let optionsBar = {
 let chart = new ApexCharts(dvChartBar, optionsBar);
 chart.render();
 
+//Mnipulacao meni desktop
 let dvMenuUserDesktop = document.querySelector("#menu-user-desktop")
 const defaultClassElementMenuDesktop = "absolute w-40 bg-white rounded-lg shadow-lg py-2 mt-16"
 const openMenuDesktop = () => {
@@ -38,6 +41,7 @@ const openMenuDesktop = () => {
   dvMenuUserDesktop.className = newClassElementMenuDesktop
 }
 
+//Manipulacao meno mobile
 let dvMenuUserMobile = document.querySelector("#menu-user-mobile")
 const defaultClassElementMenuMobile = "flex flex-col pt-4"
 let dvMenuMobileMenu = document.querySelector("#dv-menu-mobile")
@@ -59,3 +63,37 @@ const openMenuMobile = () => {
   }
   dvMenuUserMobile.className = actualClassElementMenuMobile
 }
+
+// HelloW
+
+let dvBemVIndo = document.querySelector("#bem-vindo")
+function getHello(name) {
+  axios.get(`http://verticalautomotivo.ddns.net:8080/wslap/dwCGIServer.exe/helloworld?name=${name}`)
+    .then((response) => {
+      dvBemVIndo.innerHTML = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function getEmpresas() {
+  axios.get(`http://verticalautomotivo.ddns.net:8080/wslap/dwCGIServer.exe/consulta?sql=select distinct ljcodemp from lapa19`)
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function getConsulta(sql) {
+  axios.get(`http://verticalautomotivo.ddns.net:8080/wslap/dwCGIServer.exe/consulta?sql=${sql}`)
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
